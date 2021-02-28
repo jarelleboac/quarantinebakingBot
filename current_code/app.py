1f484d34992f97b70c4437ecd04c329590077c89
Jarelle <48391432+jarelleboac@users.noreply.github.com>
2021-02-28 11:52:54 -0500

    FINAL COPY

--- current_code/app.py
+++ current_code/app.py
@@ -8,13 +8,13 @@ import requests
 
 
 app = Flask(__name__)
-ACCESS_TOKEN = ' MY TOKEN'
+ACCESS_TOKEN = 'EAANI58zW530BAEYCSGnpnonPV4ne80pbXl6JfYofrbz976puZBoSSKirGHvrIakhfh2lswZBvfxi5mPW9A4yfxHKJa3hACHADuvdUr9hOvzWy9CDIlILAGTq6shNSI9JVWZA0aDj25A1mKzjWPRF3foQxoht3RSmCi02ZAA14zvctzzwPJ8ZA'
 VERIFY_TOKEN = 'yikes'
 bot = Bot(ACCESS_TOKEN)
 
 #added spoonacular api
-url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/extract"
-querystring = {"url":"http://www.melskitchencafe.com/the-best-fudgy-brownies/"}
+url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random"
+querystring = {"number":"1","tags":"vegetarian,dessert"}
 headers = {
     'x-rapidapi-key': "6149e1a2d1mshf54b509e116c55dp17a0bdjsn9eba0c1caa76",
     'x-rapidapi-host': "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
@@ -39,14 +39,20 @@ def receive_message():
                 #Facebook Messenger ID for user so we know where to send response back to
                 recipient_id = message['sender']['id']
                 if message['message'].get('text'):
+                    #query= message['message'].get('text')
                     if(message['message'].get('text') == "I'm hungry!"):
                         response_sent_text = hungry()
                         send_message(recipient_id, response_sent_text)
-                    elif(message['message'].get('text') == "chocolate"):
+                    if(message['message'].get('text') == "I want a random recipe!"):
                         response_sent_text = get_recipe()
-                        send_message(recipient_id, response_sent_text)  
-                      
-                    else:
+                        send_message(recipient_id, response_sent_text)
+                    if(message['message'].get('text') == "Thank you!"):
+                        response_sent_text = thanks()
+                        send_message(recipient_id, response_sent_text)
+                    if(message['message'].get('text') == "ANOTHER ONE"):
+                        response_sent_text = get_recipe()
+                        send_message(recipient_id, response_sent_text)
+                    if(message['message'].get('text') == "hello!"):
                         response_sent_text = get_message()
                         send_message(recipient_id, response_sent_text)
                 #if user sends us a GIF, photo,video, or any other non-text item
@@ -63,21 +69,31 @@ def verify_fb_token(token_sent):
         return request.args.get("hub.challenge")
     return 'Invalid verification token'
 
+def thanks():
+    text= "No problem! 😋 If you want another, please type ANOTHER ONE! Otherwise, enjoy baking this new recipe. 🍴 🎉"
+
+    return text 
+
 
 #designate a specific message to send to hungry user
 def hungry():
-    text= "Let's get cooking, then!🍳Type your food preference (i.e. 'tacos' 🌮) and we will recommend some recipes. 💗"
+    text= "Let's get cooking, then!🍳Type your food preferences/ingredients separated by commas (i.e. 'apples,flour,sugar' 🌮) and we will recommend a recipe. Or say you want a random recipe! 💗"
 
     return text 
     
 def get_recipe():
     response = requests.request("GET", url, headers=headers, params=querystring)
+    #return "Here's a recipe for you 🎁 🥳!!!  " + data['title']
+    #print(response)
     data = response.json()
-    return "Here's a recipe for you 🎁 🥳!!!  " + data['title']
+    return "you got it! Here's a recipe for you 🎁 🥳!!!  " +"   \n"+"   \n"+"   \n"+data['recipes'][0]['title']+"   \n"+"   \n"+"   \n"+data['recipes'][0]['spoonacularSourceUrl']
 
 #chooses a random message to send to the user
 def get_message():
-    sample_responses = ["You are stunning!", "We're proud of you.", "Keep on being you!", "We're greatful to know you :)"]
+    sample_responses = ["Hello there 👋 , thanks for Boycotting Banana Bread! 🥰", 
+    "Welcome to Boycott Banana Bread Bot! 👋 😊", 
+    "Hello! 👋 Thanks for Boycotting Banana Bread!✨✨ Are you hungry? 🤩", 
+    "We're greatful you're here! ✨✨ Thanks for Boycotting Banana Bread! Are you hungry? 🤩"]
     # return selected item to the user
     return random.choice(sample_responses)
 
@@ -90,3 +106,4 @@ def send_message(recipient_id, response):
 
 if __name__ == "__main__":
     app.run()
+
