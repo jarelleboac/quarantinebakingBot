a96a53809704dc660f779e88d489e26ed7cbb7bf
Jarelle <48391432+jarelleboac@users.noreply.github.com>
2021-02-27 22:36:50 -0500

    can parse

--- recipe.py
+++ recipe.py
@@ -7,10 +7,12 @@ url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/extra
 querystring = {"url":"http://www.melskitchencafe.com/the-best-fudgy-brownies/"}
 
 headers = {
-    'x-rapidapi-key': "6149e1a2d1mshf54b509e116c55dp17a0bdjsn9eba0c1caa76",
+    'x-rapidapi-key': "YOUR API KEY HERE",
     'x-rapidapi-host': "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
     }
 
 response = requests.request("GET", url, headers=headers, params=querystring)
-
-print(response.text)
+data = response.json()
+print(data['title'])
+#print(data['summary'])
+#print(response.text)
